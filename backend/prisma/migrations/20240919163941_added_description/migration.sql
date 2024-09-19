4d9743a944070f91b8a42c5e756cd85ada80c3e0
Prash766 <prashantshukla766@gmail.com>
2024-09-20 00:26:58 +0530

    Add description field to blog post schema

---
+++ backend/prisma/migrations/20240919163941_added_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "description" TEXT NOT NULL DEFAULT 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia massa in magna suscipit porttitor. Proin ullamcorper posuere diam, ac blandit erat porttitor at. Sed lacus leo.';
