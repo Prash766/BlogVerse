9ced621e5a182f9869b36731aff2ad7ae1ec9230
Prash766 <prashantshukla766@gmail.com>
2024-09-20 16:33:02 +0530

    Added Suspense on Home Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,42 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LandingPage from "./pages/landingPage/LandingPage"
-import Login from "./pages/Auth/Login"
-import Signup from "./pages/Auth/Signup"
-import Home from "./pages/Home/Home"
-import CreateBlog from "./pages/NewBlog/CreateBlog"
-import ProtectedRoutes from "./utils/ProtectedRoutes"
-import ProfilePage from "./pages/Profile/ProfilePage"
-import BlogPage from "./pages/Blog/BlogPage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/landingPage/LandingPage";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Home from "./pages/Home/Home";
+import CreateBlog from "./pages/NewBlog/CreateBlog";
+import ProtectedRoutes from "./utils/ProtectedRoutes";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import BlogPage from "./pages/Blog/BlogPage";
+import { Suspense } from "react";
+import { Skeleton } from "./components/ui/skeleton";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<LandingPage/>} />
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      <Route element={<ProtectedRoutes/>}>
-      <Route path='/home' element={<Home/>} />
-      <Route path='/write' element={<CreateBlog/>}/>
-      <Route path='/profile' element={<ProfilePage/>} />
-      <Route path='/blog/:id' element={<BlogPage/>} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route
+              path="/home"
+              element={
+                <Suspense fallback={<Skeleton />}>
+                  {" "}
+                  <Home />
+                </Suspense>
+              }
+            />
+
+            <Route path="/write" element={<CreateBlog />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/blog/:id" element={<BlogPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
